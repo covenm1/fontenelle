4f0b368288176d0405738fe409f9a7969d7beac3
bangarang <me@alexjrock.com>
2015-11-25 12:37:38 -0600

    Typo on the careers page

--- components/pages/careers/index.jsx
+++ components/pages/careers/index.jsx
@@ -23,7 +23,7 @@ var Job = React.createClass({
       <div className="job">
         <h2 className="job_headline" dangerouslySetInnerHTML={{__html: title}}></h2>
         { description ? <div className="job_description " dangerouslySetInnerHTML={{__html: description}}></div> : null }
-        { descriptionUrl ? <a className="job_description_url" href={descriptionUrl} target="_blank"><p>Learn More About the Postition</p></a> : null }
+        { descriptionUrl ? <a className="job_description_url" href={descriptionUrl} target="_blank"><p>Learn More About the Position</p></a> : null }
         { contact ? <p className="job_contact"><span className="job_label">Please contact: </span><a href={contact}>{contact}</a></p> : null }
         { formatted_deadline ? <p className="job_formatted_deadline"><span className="job_label">Application Deadline: </span>{formatted_deadline}</p> : null }
       </div>
