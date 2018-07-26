4e43a82887faaa41189c6a9f0603b36586d28a8c
Alex Rock <hi@lxrck.com>
2018-07-26 12:55:19 -0500

    new redirect

--- routes/index.js
+++ routes/index.js
@@ -84,7 +84,7 @@ module.exports = function(app) {
 	});
 
   app.get('/donateraptorrecovery', function(req, res) {
-    res.redirect('8913.blackbaudhosting.com/8913/Raptor-Recovery-Individual');
+    res.redirect('https://8913.blackbaudhosting.com/8913/Raptor-Recovery-Individual');
   });
 
   app.get('/giving', function(req, res) {
